b56f771fe490c7cf65ad23a3ee29689676206ff4
Murali Peddi <142522630+muralipeddi@users.noreply.github.com>
2025-12-06 11:02:13 -0500

    Delete src directory

--- src/tb_coap.h
+++
@@ -1,4 +0,0 @@
-void start_tb_coap();
-int tb_coap_send_telemetry_int(const char * name, int value);
-int tb_coap_send_telemetry_double(const char * name, double value);
-int tb_coap_send_telemetry_payload_string(const char * name, const char * payload);
