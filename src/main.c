9c2afde3e7f5b72d390fa85260f612395d5ba0f1
Murali Peddi <142522630+muralipeddi@users.noreply.github.com>
2025-12-20 15:53:08 -0500

    Remove copyright notice from src/main.c
    Removed copyright notice from main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,3 @@
-/* Copyright (c) 2022 Nordic Semiconductor ASA
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
- */
-
 #include "application.h"
 #include "connection.h"
 #include "led_control.h"
