94931e4607f28cbd36d81fbb979397844a022eb0
Murali Peddi <142522630+muralipeddi@users.noreply.github.com>
2025-12-20 15:53:17 -0500

    Remove copyright notice from vibration.c
    Removed copyright notice from vibration.c

--- src/vibration.c
+++ src/vibration.c
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2022 Nordic Semiconductor ASA
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
- */
-
 #include <math.h>
 #include <stdlib.h>
 #include <zephyr/devicetree.h>
